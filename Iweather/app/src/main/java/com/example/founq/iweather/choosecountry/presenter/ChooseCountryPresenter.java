164a2d8ebe244da5e9bbe44901362f7ce4716f03
孙瑞环 <sunruihuan@founq.com>
2019-04-09 18:46:11 +0800

    country presenter中的asynctask

--- Iweather/app/src/main/java/com/example/founq/iweather/choosecountry/presenter/ChooseCountryPresenter.java
+++ Iweather/app/src/main/java/com/example/founq/iweather/choosecountry/presenter/ChooseCountryPresenter.java
@@ -3,37 +3,47 @@ package com.example.founq.iweather.choosecountry.presenter;
 import android.os.AsyncTask;
 
 import com.example.founq.iweather.base.BasePresenter;
+import com.example.founq.iweather.choosecity.presenter.ChooseCityPresenter;
 import com.example.founq.iweather.choosecountry.CountryContract;
 import com.example.founq.iweather.choosecountry.model.ChooseCountryModel;
 import com.example.founq.iweather.choosecountry.view.ChooseCountryActivity;
 import com.example.founq.iweather.data.Utility;
 
-public class ChooseCountryPresenter extends BasePresenter<ChooseCountryActivity> implements CountryContract.CountryPresenterInterface{
+import java.lang.ref.WeakReference;
+
+public class ChooseCountryPresenter extends BasePresenter<ChooseCountryActivity> implements CountryContract.CountryPresenterInterface {
 
 
     CountryContract.CountryModelInterface chooseCountryModel = new ChooseCountryModel();
-    private String strOther;
 
     @Override
-    public void getModel(int code,int citycode) {
+    public void getModel(int code, int citycode) {
 
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                strOther = chooseCountryModel.getCountry(code,citycode);
-                return null;
+        CountryAsyncTask mCountryAsyncTask = new CountryAsyncTask(ChooseCountryPresenter.this);
+        mCountryAsyncTask.execute(code, citycode);
+    }
+
+    private static class CountryAsyncTask extends AsyncTask<Integer, Void, Boolean> {
+
+        private WeakReference<ChooseCountryPresenter> mPresenterWeakReference;
+        private String strOther;
+
+        public CountryAsyncTask(ChooseCountryPresenter presenter) {
+            mPresenterWeakReference = new WeakReference<>(presenter);
+        }
+
+        @Override
+        protected Boolean doInBackground(Integer... integers) {
+            strOther = mPresenterWeakReference.get().chooseCountryModel.getCountry(integers[0], integers[1]);
+            return Utility.handleCountyResponse(strOther, integers[1]);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if (aBoolean) {
+                mPresenterWeakReference.get().view.get().show();
             }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                if (Utility.handleCountyResponse(strOther, citycode)) {
-                    view.get().show();
-                }
-            }
-
-        }.execute();
-
-
+        }
     }
 }
